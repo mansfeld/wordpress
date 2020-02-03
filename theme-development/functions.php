01bf62e32e002727142a23491c5135028c5932ad
Paweł Mansfeld <pawel@mansfeld.pl>
2020-02-03 17:32:41 +0100

    Create functions.php

---
+++ theme-development/functions.php
@@ -0,0 +1,40 @@
+<?php 
+/* Basics */
+
+
+/* Main menu initialization */
+
+function reg_mymenu(){
+	register_nav_menu('header-menu', __('Main Menu'));
+}
+add_action('init', 'reg_mymenu');
+
+
+
+/* Custom logo support */
+
+add_theme_support('custom-logo');
+
+
+
+/* Post Thumbnails support */
+
+add_theme_support('post-thumbnails', array('post', 'page'));
+
+
+
+/* Custom widget support */
+
+function custom_widget_init(){
+	register_sidebar(
+		array(
+			'name'          => 'Custom-widget',
+			'id'            => 'custom-widget',
+			'before_widget' => ' ',
+			'after_widget'  => ' ',
+			'before_title'  => '<h4>',
+			'after_title'   => '</h4>',
+		)
+	);
+}
+add_action('widgets_init', 'custom_widget_init');
