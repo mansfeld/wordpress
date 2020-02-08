ea547e053cfcf366893d5a0a4c2cf98134891e08
Paweł Mansfeld <pawel@mansfeld.pl>
2020-02-08 17:07:45 +0100

    Create index.php

---
+++ theme-development/index.php
@@ -0,0 +1,12 @@
+<?php
+  get_template_part("partials/header");
+  
+  <main>
+  <?php if(have_posts()) : ?>
+    <?php while(have_posts()) : the_post(); ?> 
+      <?php the_content(); ?>
+    <?php endwhile; ?>
+  <?php endif; ?> 
+  </main>
+
+  get_template_part("partials/footer");
