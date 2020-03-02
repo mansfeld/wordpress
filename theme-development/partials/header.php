01dcc23af37b940477ec0b7b4421bc31c5e134a4
Paweł Mansfeld <pawel@mansfeld.pl>
2020-03-02 17:37:05 +0100

    Update header.php

--- theme-development/partials/header.php
+++ theme-development/partials/header.php
@@ -3,3 +3,6 @@
 $custom_logo_id = get_theme_mod( 'custom_logo' );
 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 $image_url = $image[0];
+?>
+
+<img src="<?php echo $image_url; ?>" alt="<?php bloginfo("url") ?>">
