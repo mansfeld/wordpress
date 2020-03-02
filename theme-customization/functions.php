c5b6e05cf8d55f33c6869cc7e9c980cb0be5c91f
Paweł Mansfeld <pawel@mansfeld.pl>
2020-03-02 14:07:42 +0100

    Create functions.php

---
+++ theme-customization/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Remove Comments */
+
+function remove_comments_from_admin_menus() {
+    remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'remove_comments_from_admin_menus' );
+
+
+function remove_comments_post_and_pages() {
+    remove_post_type_support( 'post', 'comments' );
+    remove_post_type_support( 'page', 'comments' );
+}
+add_action('init', 'remove_comments_post_and_pages', 100);
+
+
+function remove_comments_from_bar() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+}
+add_action( 'wp_before_admin_bar_render', 'remove_comments_from_bar' );
