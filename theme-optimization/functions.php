5f63664d1e51afc03d716bf3893722c4d4d7c1ef
Paweł Mansfeld <pawel@mansfeld.pl>
2020-03-09 22:03:39 +0100

    Update functions.php

--- theme-optimization/functions.php
+++ theme-optimization/functions.php
@@ -5,14 +5,11 @@ add_filter('jpeg_quality', function($arg){return 80;});
 
 /* Remove Contact Form 7 front-end assets from every page instead of Contact */
 function remove_cf7_assets(){
-        wp_reset_query();
-        if(!is_page("contact")){
-            add_filter( 'wpcf7_load_js', '__return_false' ); 
-			add_filter( 'wpcf7_load_css', '__return_false' );
-        }
-        else{
-           /* Nothing to do */
-        }  
+	wp_reset_query();
+	if(!is_page("contact")){
+		add_filter( 'wpcf7_load_js', '__return_false' ); 
+		add_filter( 'wpcf7_load_css', '__return_false' );
+	}  
 }
 add_action("wp", "remove_cf7_assets");	
 
